a602eddbcdf3e6b8ae4c9e0bbe5aed07dedab331
Uzdowski <sarafiel@tlen.pl>
2015-08-12 15:34:51 +0200

    Pulsar

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public float rotationsPerMinute = 10;
+
+	void Update () {
+		transform.Rotate(0,0,-6.0f*rotationsPerMinute*Time.deltaTime,0);
+	}
+}
